ee713e735fc947a529940bc3028ebdc966ea6675
Nikolai Filatov <talai-2005@yandex.ru>
2019-11-28 19:32:14 +0300

    Initial commit

---
+++ src/main/java/EpamCollections/OptionaTaskOne.java
@@ -0,0 +1,31 @@
+package EpamCollections;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * 1.   Ввести строки из файла, записать в список. Вывести строки в файл в обратном порядке.
+ */
+
+public class OptionaTaskOne {
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> list = new ArrayList<>();
+        File myfile = new File("Sourcefile.txt");
+        FileReader fr = new FileReader(myfile);
+        BufferedReader reader = new BufferedReader(fr);
+        String line = null;
+
+        while (reader.ready()) {
+            list.add(reader.readLine());
+        }
+
+        Collections.reverse(list);
+        for (String element : list) {
+            System.out.println(element);
+        }
+    }
+}
---
+++ src/main/java/EpamCollections/OptionalTaskTwo.java
@@ -0,0 +1,27 @@
+package EpamCollections;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class OptionalTaskTwo {
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> list = new ArrayList<String>();
+        File myfile = new File("Sourcefile.txt");
+        FileReader fr = new FileReader(myfile);
+        BufferedReader reader = new BufferedReader(fr);
+        String line = null;
+
+        while (reader.ready()) {
+            list.add(reader.readLine());
+        }
+
+        Collections.sort(list);
+        for (String element : list) {
+            System.out.println(element);
+        }
+    }
+}
